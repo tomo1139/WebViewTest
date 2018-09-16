7a3a916055ce1575978d817e2c91a55a9ca59794
beta1139 <beta1139@gmail.com>
2018-09-16 13:49:34 +0900

    no message

---
+++ WebViewTest/app/src/main/java/com/example/tomo/webviewtest/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.tomo.webviewtest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("http://www.google.com/")
+    }
+}
